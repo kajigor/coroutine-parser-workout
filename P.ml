158be8edf4644556ed0958e4c90b23c7e44b3341
kajigor <kajigor@gmail.com>
2016-12-26 03:34:22 +0300

    Monadic seq

--- P.ml
+++ P.ml
@@ -325,4 +325,314 @@ module CPS =
       
       run test09 "1-2+0";
   end
-  
+
+module MonadicCPS =
+  struct
+
+    (* Result of "one-step" parsing *)
+    type 'a result = 
+    (*          parsed residual    residual parsing
+                value   stream        steps
+                   |      |             |
+                   v      v             v
+    *)
+    | Ok       of 'a * stream * (unit -> 'a result) 
+    (*             residual parsing
+                        steps
+                          |
+                          v
+    *)
+    | Continue of (unit -> 'a result)   
+    | End 
+
+    let rec maps f k () = 
+      match k () with
+      | Ok (t, s, k) -> Ok (f t, s, maps f k)
+      | Continue k   -> Continue (maps f k)
+      | End          -> End
+
+    (* One-step parser *)
+    type 'a p = stream -> 'a result
+
+    (* Some tracing noodles *)
+    let log = ref false
+    let log_on  () = log := true
+    let log_off () = log := false
+
+    let ticker () =
+      let i = ref 0 in
+      (fun s -> 
+         if !log then (
+            Printf.printf "%s %d\n%!" s !i;
+           incr i;
+           ignore (read_line ())
+        )
+      )
+
+    (* Empty residual parsing steps *)
+    let stop () = End
+
+    (* Some primitive parsers *)
+    let eof = function [] -> Ok ((), [], stop) | _ -> End
+
+    let get_eof = (fun x s -> maps (fun _ -> x) (fun () -> eof s) ())
+
+    let empty s = Ok ((), s, stop)
+
+    let sym x = function
+    | y::tl when y = x -> Ok (x, tl, stop)
+    | _ -> End
+
+    let seq a b s =
+      let rec inner ka kb () =
+        match kb () with
+        | Ok (tb, sb, kb) -> Ok (tb, sb, inner kb ka)
+        | Continue kb     -> Continue (inner kb ka)
+        | End             -> Continue ka
+      in
+      Continue (
+        let rec outer ka kb () =
+           match ka () with
+           | Ok (ta, sa, ka) -> Continue (outer ka (inner kb (fun () -> b ta sa)))
+           | Continue ka     -> Continue (inner (outer ka stop) kb)
+           | End             -> Continue kb
+        in
+        outer (fun () -> a s) stop
+      )
+
+    let alt a b s = 
+      let t = ticker () in
+      let rec inner a b () =
+        match a () with
+        | Ok (ta, sa, ka) -> 
+            t ("alt:" (*^ show(tree) ta*));
+            Ok (ta, sa, inner b ka)
+        | Continue ka -> 
+            t "alt:continue"; 
+            Continue (inner b ka)
+        | End -> 
+            t "alt:end"; 
+            b ()
+      in
+      Continue (inner (fun () -> a s) (fun () -> b s))
+
+    (* Apply gets the first result *)
+    let apply p s =
+      (* let t = ticker () in *)
+      let rec inner p =
+        match p () with
+        | Ok (tp, sp, kp) -> Some tp
+        | Continue kp     -> inner kp
+        | End             -> None
+      in
+      inner (fun () -> p s)
+
+    let (|>) = seq
+    let (<>) = alt
+
+    let a = sym 'a'
+    let b = sym 'b'
+    let c = sym 'c'
+
+    let test00 =
+         a |> (fun x s -> maps (fun y -> `Pair (x, y)) (fun () -> b s) ()) 
+      <> (fun s -> maps (fun c -> `Single c) (fun () -> c s) ())
+
+    let test01 = 
+         a 
+      |> (fun x     s -> maps (fun y -> x,y  ) (fun () -> b s) ())
+      |> (fun (x,y) s -> maps (fun z -> x,y,z) (fun () -> c s) ())
+      |> get_eof
+
+    let test02 = 
+         (a <> b <> c) |> get_eof
+
+    let test03 = 
+      let rec pali s =
+       (
+          (a 
+          |> (fun x s -> maps (fun y -> x,y) (fun () -> pali s) ()) 
+          |> (fun (l,p) s -> maps (fun r -> `T (l,p,r)) (fun () -> a s) ())
+          )
+       <> (b 
+          |> (fun x s -> maps (fun y -> x,y) (fun () -> pali s) ()) 
+          |> (fun (l,p) s -> maps (fun r -> `T (l,p,r)) (fun () -> b s) ()) 
+          )
+       <> (c
+          |> (fun x s -> maps (fun y -> x,y) (fun () -> pali s) ()) 
+          |> (fun (l,p) s -> maps (fun r -> `T (l,p,r)) (fun () -> c s) ()) 
+          )
+       <> (fun s -> maps (fun _ -> `E) (fun () -> empty s) ())
+       ) s
+      in pali |> get_eof
+
+     let test04 = 
+       let rec expr s = 
+        ( 
+           (expr 
+           |> (fun x s -> maps (fun _ -> x) (fun () -> sym '+' s) ())
+           |> (fun x s -> maps (fun y -> `Sum (x,y)) (fun () -> expr s) ())
+           )
+        <> (fun s -> maps (fun x -> `Primary x) (fun () -> a s) ())
+        ) s
+       in expr |> get_eof
+    
+    let test05 = 
+      let rec inner s = 
+        (
+           inner |> (fun x s -> maps (fun y -> y::x) (fun () -> a s) ())
+        <> (fun s -> maps (fun _ -> []) (fun () -> empty s) ())
+        ) s
+      in inner |> get_eof 
+
+    let test06 = 
+      let rec addi s = 
+        (
+           mulli
+        <> (addi 
+           |> (fun x s -> maps (fun _ -> x) (fun () -> sym '+' s) ()) 
+           |> (fun x s -> maps (fun y -> `Sum (x, y)) (fun () -> mulli s) ()) 
+           )
+        ) s
+      and mulli s = 
+        (
+           primary
+        <> (mulli 
+           |> (fun x s -> maps (fun _ -> x) (fun () -> sym '*' s) ()) 
+           |> (fun x s -> maps (fun y -> `Mul (x, y)) (fun () -> primary s) ()) 
+           )
+        ) s
+      and primary = 
+        fun s -> maps (fun x -> `Primary x) (fun () -> (a <> b <> c) s) () 
+      in addi |> get_eof
+
+    let expr n = 
+      let rec e s = 
+        (
+           m 
+        <> (e 
+           |> (fun x s -> maps (fun op -> x,op) (fun () -> (sym '+' <> sym '-') s) ()) 
+           |> (fun (x,op) s -> maps (fun y -> `BinOp (op, x, y)) (fun () -> m s) ())
+           )
+        ) s
+      and m s = 
+        (
+           p 
+        <> (m 
+           |> (fun x s -> maps (fun op -> x,op) (fun () -> (sym '*' <> sym '/') s) ()) 
+           |> (fun (x,op) s -> maps (fun y -> `BinOp (op, x, y)) (fun () -> p s) ())
+           )
+        ) s
+      and p s = 
+        (
+           n
+        <> (sym '(' 
+           |> (fun _ s -> maps (fun e -> `Bracket e) (fun () -> e s) ()) 
+           |> (fun x s -> maps (fun _ -> x) (fun () -> sym ')' s) ())
+           )
+        ) s 
+      in e |> get_eof
+
+    let test07 = expr (fun s -> maps (fun x -> `Primary x) (fun () -> (a <> b <> c) s) ())
+    
+    let test08 = expr (fun s -> maps (fun x -> `Primary x) (fun () -> sym 'n' s) ())
+(*
+    let integer = 
+      let nzdigit   = List.fold_left (fun acc x -> acc <> sym x) (sym '1') (of_string "23456789") in
+      let digit     = sym '0' <> nzdigit in 
+      let rec num s = ((digit |> num) <> empty) s in
+      (nzdigit |> num) <> (sym '0')
+    
+    let test09 = expr integer
+*)
+    let run printer test input =
+      Printf.printf "%s\n%!" @@
+      show(option) printer (apply test @@ of_string input)
+
+    let runtest00 =
+      let print = function
+      | `Pair (x, y) -> Printf.sprintf "%c %c" x y
+      | `Single x    -> Printf.sprintf "%c" x
+      in run print test00
+
+    let runtest01 = 
+      let print (a,b,c) = Printf.sprintf "%c %c %c" a b c
+      in run print test01
+      
+    let runtest02 = 
+      let print = Printf.sprintf "%c"
+      in run print test02
+    
+    let runtest03 = 
+      let rec print = function 
+      | `E -> ""
+      | `T (l, p, r) -> Printf.sprintf "%c [%s] %c" l (print p) r
+      in run print test03
+
+    let print_expr print_primary = 
+      let rec print = function
+      | `Primary x -> print_primary x
+      | `Sum (x,y) -> Printf.sprintf "%s + %s" (print x) (print y)
+      | `Mul (x,y) -> Printf.sprintf "%s * %s" (print x) (print y)
+      in print 
+
+    let print_expr' print_primary = 
+      let rec print = function
+      | `Primary x -> print_primary x
+      | `BinOp (op,x,y) -> Printf.sprintf "%s %c %s" (print x) op (print y)
+      | `Bracket e -> Printf.sprintf "(%s)" (print e)
+      in print 
+
+    let runtest04 = run (print_expr (Printf.sprintf "%c")) test04
+
+    let runtest05 = 
+      let print x = List.fold_left (fun acc _ -> "a" ^ acc) "" x
+      in run print test05
+
+    let runtest06 = run (print_expr (Printf.sprintf "%c")) test06
+
+    let runtest07 = run (print_expr' (Printf.sprintf "%c")) test07
+    
+    let runtest08 = run (print_expr' (fun _ -> "n")) test08
+
+    let _ = 
+      Printf.printf "Running CPS-style:\n%!";
+      
+      runtest00 "ab";
+      runtest00 "c";
+      runtest00 "d";
+
+      runtest01 "abc";
+      runtest01 "abcd";
+      runtest02 "a";
+      runtest02 "b";
+      runtest02 "c";
+
+      runtest03 "";
+      runtest03 "abcccabaabaaabbbccccccbbbaaabaabacccba";
+      runtest03 "abccbbaccbacaabbbbaabbbbaacabccabbccba";
+      runtest03 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+
+      runtest04 "a";
+      runtest04 "a+a";
+
+      runtest05 "aaaaa";
+
+      runtest06 "a";
+      runtest06 "b";
+      runtest06 "c";
+      runtest06 "a+a+a";        
+      runtest06 "a*b+c";
+      
+      runtest07 "a*(b+c)";
+      
+      runtest08 "n*(n+n)";
+      runtest08 "n/(n*(n+n)-n+n*n/(n-n*n+n))";
+
+      (* runtest08 "(n+(n+(n+(n+(n+(n+n))))))"; *)
+(*      
+      runtest09 "1-2+0";
+*)
+  end
+
